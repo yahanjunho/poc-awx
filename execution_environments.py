1416b9387ee7715152218b1c2e3181cf4a039e42
yahanjunho <yahanjunho@gmail.com>
2022-12-29 03:09:09 +0000

    update, awx-03

---
+++ execution_environments.py
@@ -0,0 +1,4 @@
+GLOBAL_JOB_EXECUTION_ENVIRONMENTS = [
+    {'name': 'test-environment' , 'image': '192.168.50.137:5000/awx-ee:latest'},
+]
+CONTROL_PLANE_EXECUTION_ENVIRONMENT = "192.168.50.137:5000/awx-ee:latest"
